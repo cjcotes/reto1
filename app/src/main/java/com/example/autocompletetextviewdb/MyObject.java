8d85227e1dad4c1560a0aa6d8d15b4fd8022f71d
ubuntu7 <cjcotes@gmail.com>
2016-04-11 09:02:34 -0500

    Inicio

---
+++ app/src/main/java/com/example/autocompletetextviewdb/MyObject.java
@@ -0,0 +1,14 @@
+package com.example.autocompletetextviewdb;
+
+
+public class MyObject {
+
+    public String objectName;
+
+    // constructor for adding sample data
+    public MyObject(String objectName){
+
+        this.objectName = objectName;
+    }
+
+}
